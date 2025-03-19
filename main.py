93cd9820606b073b19137ca03deb3f970df837bf
Bin0007 <122222621+Bin0007@users.noreply.github.com>
2025-03-19 03:21:07 -0300

    background pronto

--- main.py
+++ main.py
@@ -7,3 +7,23 @@ from views import *
 
 if __name__ == "__main__":
     app.run()
+
+#from flask import Flask
+#import os # os.walk()
+
+#app = Flask("checkboxes") # from flask
+
+#def checkboxes(directory):
+    #all_names = ""
+    #template = "{}: <input type=\"checkbox\" value=\"{}\"/><br>\n"
+
+    #for root, dirs, files in os.walk(directory):
+        #for name in dirs:
+            #all_names += template.format(name) # name vai para {0}.
+    #return all_names
+
+#@app.route("/")
+#def main():
+    #return checkboxes(".") # retorna all_names para a página.
+
+#app.run()
